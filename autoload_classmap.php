f54c3e14e66152ececc1db9e23336bd24993c2c8
Luis Montealegre <montealegreluis@gmail.com>
2012-11-19 10:35:06 -0600

    Agrego classmap del módulo

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,2 +1,6 @@
 <?php
-return array();
+return array(
+    'BookStore\Controller\AuthorController' => __DIR__ . '/src/BookStore/Controller/AuthorController.php',
+    'BookStore\Model\Author' => __DIR__ . '/src/BookStore/Model/Author.php',
+    'BookStore\Model\AuthorTable' => __DIR__ . '/src/BookStore/Model/AuthorTable.php',
+);
